75f09cd46241df5899aa35ca5d0d629e7e788ce2
LATIA112-1 <144101859+LATIA112-1@users.noreply.github.com>
2023-11-28 20:47:02 +0800

    Add files via upload

---
+++ pie chart.py/hello-pie-chart 圖表一/data.js
@@ -0,0 +1,11 @@
+let evaluation_ratio = [
+    {
+        "name": "男",
+        "count": 2
+    },
+    {
+        "name": "女",
+        "count": 67
+    }
+];
+
---
+++ pie chart.py/hello-pie-chart 圖表二/main.js
@@ -0,0 +1,29 @@
+let myGraph = document.getElementById('myGraph');
+let trace1 ={};
+trace1.type ="pie";
+trace1.title ="年級比";
+trace1.labels =[];
+trace1.values =[];
+trace1.domain = {
+    row:0,
+    column:0
+};
+
+for (let x=0; x<evaluation_ratio.length; x++){
+    trace1.labels[x] = evaluation_ratio[x]['name'];
+    trace1.values[x] = evaluation_ratio[x]['count'];
+} 
+
+let data = [];
+data.push(trace1);
+
+
+let layout ={
+    margin:{
+        t:10,
+        l:0,
+    }
+    
+};
+
+Plotly.newPlot(myGraph, data, layout);
