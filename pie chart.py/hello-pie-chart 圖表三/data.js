ab2d5221630968b700fd1df32e807fbc957e9843
LATIA112-1 <144101859+LATIA112-1@users.noreply.github.com>
2023-11-28 20:48:10 +0800

    Add files via upload

---
+++ pie chart.py/hello-pie-chart 圖表三/data.js
@@ -0,0 +1,34 @@
+let evaluation_ratio = [
+    {
+        "name": "中國",
+        "count": 15
+    },
+    {
+        "name": "越南",
+        "count": 40
+    },
+    {
+        "name": "印尼",
+        "count": 8
+    },
+    {
+        "name": "菲律賓",
+        "count": 1
+    },
+    {
+        "name": "泰國",
+        "count": 2
+    },
+    {
+        "name": "日本",
+        "count": 1
+    },
+    {
+        "name": "美國",
+        "count": 1
+    },
+    {
+        "name": "南韓",
+        "count": 1
+    }
+];
---
+++ pie chart.py/hello-pie-chart 圖表二/data.js
@@ -0,0 +1,14 @@
+let evaluation_ratio = [
+    {
+        "name": "一年級",
+        "count": 24
+    },
+    {
+        "name": "二年級",
+        "count": 21
+    },
+    {
+        "name": "三年級",
+        "count": 24
+    }
+];
